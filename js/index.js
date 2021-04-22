5eba42f853a6add6dd2a1702b7a3761d26e83efd
Sanusi Hassan <sanusihassan122@gmail.com>
2021-04-22 13:37:50 +0300

    project created

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function nothing() {
+    /**
+     * nothing is happening here
+     * just
+     * a
+     * bunch
+     * of
+     * comments
+     */
+}
+
+nothing();
+
+function scrollTop() {
+    window.scroll(0, 0);
+}
+
+scrollTop();
+
+/**
+ * ststing git stash
+ * trying stash options
+ */
